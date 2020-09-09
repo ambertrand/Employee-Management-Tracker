5e7243e79b6c6cdfd73e3b980876562d486776e2
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-09-08 21:26:53 -0400

    creating database sql file

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const cTable = require("console.table");
 
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 6060,
+//     user: "root",
+//     password: "Alex-3891!",
+//     database: "employee_mgmtdb"
+// });
+
+// connection.connect(function (err) {
+//     if (err) throw err;
+//     start();
+// });
+
+start = () => {
+    inquirer
+    .prompt({
+        name: "startFunc",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees by Department",
+            "View All Employees by Manager",
+            "Add Employee",
+            "Add Department",
+            "Add Role",
+            "Update Employee Role"
+        ]
+    })
+}
+
+start();
